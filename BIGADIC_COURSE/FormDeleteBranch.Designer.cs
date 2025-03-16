829c673cb1ef67d7862304ded304d0c2c5543a43
Emre Cüni <cuniiemre@gmail.com>
2025-03-16 11:35:49 +0300

    delete branch added

---
+++ BIGADIC_COURSE/FormDeleteBranch.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace BIGADIC_COURSE
+{
+    partial class FormDeleteBranch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDeleteBranch));
+            this.flowLayoutPanelBranches = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBoxBranches = new System.Windows.Forms.GroupBox();
+            this.groupBoxDelete = new System.Windows.Forms.GroupBox();
+            this.buttonDeleteBranch = new System.Windows.Forms.Button();
+            this.groupBoxBranches.SuspendLayout();
+            this.groupBoxDelete.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanelBranches
+            // 
+            this.flowLayoutPanelBranches.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelBranches.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.flowLayoutPanelBranches.Location = new System.Drawing.Point(3, 21);
+            this.flowLayoutPanelBranches.Name = "flowLayoutPanelBranches";
+            this.flowLayoutPanelBranches.Size = new System.Drawing.Size(418, 347);
+            this.flowLayoutPanelBranches.TabIndex = 0;
+            // 
+            // groupBoxBranches
+            // 
+            this.groupBoxBranches.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxBranches.Controls.Add(this.flowLayoutPanelBranches);
+            this.groupBoxBranches.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxBranches.Name = "groupBoxBranches";
+            this.groupBoxBranches.Size = new System.Drawing.Size(424, 371);
+            this.groupBoxBranches.TabIndex = 1;
+            this.groupBoxBranches.TabStop = false;
+            this.groupBoxBranches.Text = "Branşlar:";
+            // 
+            // groupBoxDelete
+            // 
+            this.groupBoxDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxDelete.Controls.Add(this.buttonDeleteBranch);
+            this.groupBoxDelete.Location = new System.Drawing.Point(12, 387);
+            this.groupBoxDelete.Name = "groupBoxDelete";
+            this.groupBoxDelete.Size = new System.Drawing.Size(424, 64);
+            this.groupBoxDelete.TabIndex = 2;
+            this.groupBoxDelete.TabStop = false;
+            // 
+            // buttonDeleteBranch
+            // 
+            this.buttonDeleteBranch.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDeleteBranch.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonDeleteBranch.Location = new System.Drawing.Point(7, 14);
+            this.buttonDeleteBranch.Name = "buttonDeleteBranch";
+            this.buttonDeleteBranch.Size = new System.Drawing.Size(408, 44);
+            this.buttonDeleteBranch.TabIndex = 0;
+            this.buttonDeleteBranch.Text = "Sil";
+            this.buttonDeleteBranch.UseVisualStyleBackColor = true;
+            this.buttonDeleteBranch.Click += new System.EventHandler(this.buttonDeleteBranch_Click);
+            // 
+            // FormDeleteBranch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(448, 463);
+            this.Controls.Add(this.groupBoxDelete);
+            this.Controls.Add(this.groupBoxBranches);
+            this.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormDeleteBranch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Branş Sil";
+            this.Load += new System.EventHandler(this.FormDeleteBranch_Load);
+            this.groupBoxBranches.ResumeLayout(false);
+            this.groupBoxDelete.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelBranches;
+        private System.Windows.Forms.GroupBox groupBoxBranches;
+        private System.Windows.Forms.GroupBox groupBoxDelete;
+        private System.Windows.Forms.Button buttonDeleteBranch;
+    }
+}
---
+++ BIGADIC_COURSE/FormDeleteBranch.cs
@@ -0,0 +1,133 @@
+﻿using BIGADIC_COURSE.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BIGADIC_COURSE
+{
+    public partial class FormDeleteBranch : Form
+    {
+        public FormDeleteBranch()
+        {
+            InitializeComponent();
+        }
+
+        StringBuilder query = new StringBuilder();
+        List<SqlParameter> parameters = new List<SqlParameter>();
+
+        private void FormDeleteBranch_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                GetCourses();
+            }
+            catch (Exception ex)
+            {
+                Log.logger.Error($"FormDeleteBranch_Load Error Hata Kodu: 5000 ex.message: {ex.Message} ex.stacktrace: {ex.StackTrace}");
+                MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 5000", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void buttonDeleteBranch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Sql sql = new Sql();
+
+                List<CheckBox> checkboxes = flowLayoutPanelBranches.Controls.OfType<CheckBox>().Where(c => c.Checked == true).ToList();
+
+                //query.Clear();
+                //query.Append("DELETE FROM COURSES WHERE ID IN (");
+
+                //for (int i = 0; i < checkboxes.Count; i++)
+                //{
+                //    query.Append($"@ID{i + 1}");
+                //    parameters.Add(new SqlParameter($"@ID{i + 1}", SqlDbType.Int) { Value = checkboxes[i].Tag });
+                //    if (i < checkboxes.Count - 1)
+                //        query.Append(',');
+                //}
+                //query.Append(");");
+
+                query.Clear();
+                parameters.Clear();
+                for (int i = 0; i < checkboxes.Count; i++)
+                {
+                    query.Append($"IF NOT EXISTS (SELECT * FROM COURSEREGISTER WHERE COURSE_ID = @Id{i + 1}) " +
+                        $"AND NOT EXISTS (SELECT * FROM PERSONELS WHERE COURSE_ID = @Id{i + 1}) " +
+                        "BEGIN " +
+                        $"DELETE FROM COURSES WHERE ID = @Id{i + 1} " +
+                        "END " +
+                        "ELSE " +
+                        "BEGIN " +
+                        "SELECT 'MEVCUT KAYIT BULUNMAKTADIR' " +
+                        "END ");
+                    parameters.Add(new SqlParameter($"@Id{i + 1}", SqlDbType.Int) { Value = checkboxes[i].Tag });
+                }
+
+                int returnCode = await sql.EditData(query.ToString(), parameters);
+                switch (returnCode)
+                {
+                    case 0:
+                        Log.logger.Error("buttonDeleteBranch_Click Error Sql Bağlantısı Açılamadı. Hata Kodu: 5002");
+                        MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 5002", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    case 1:
+                        Log.logger.Info($"Seçilen kurs başarıyla silindi.");
+                        GetCourses();
+                        MessageBox.Show($"Seçilen Kurs(lar) Başarıyla Silindi.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+                    case 2:
+                        Log.logger.Warn("Bu kursa kayıtlı öğrenci/Personel bulunmaktadı.");
+                        MessageBox.Show("Bu Kursa Kayıtlı Öğrenci/Personel Bulunmaktadır.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        break;
+                    case -1:
+                        Log.logger.Error("buttonDeleteBranch_Click Error EditData Hatası. Hata Kodu: 5003");
+                        MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 5003", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.logger.Error($"buttonDeleteBranch_Click Error Hata Kodu: 5001 ex.message: {ex.Message} ex.stacktrace: {ex.StackTrace}");
+                MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 5001", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void GetCourses()
+        {
+            try
+            {
+                Sql sql = new Sql();
+
+                query.Clear();
+                query.Append("SELECT * FROM COURSES;");
+
+                DataTable branchesTable = await sql.GetFromDb(query.ToString()); // veri tabanından bütün branşlar çekilir
+
+                flowLayoutPanelBranches.Controls.Clear();
+                foreach (DataRow branch in branchesTable.Rows) // veri tabanından çekilen bütün branşlar flowlayoutpanel'a eklenir
+                {
+                    CheckBox addedBranch = new CheckBox();
+                    flowLayoutPanelBranches.Controls.Add(addedBranch);
+                    addedBranch.Text = branch.ItemArray[1].ToString();
+                    addedBranch.Name = $"checkBox{branch.ItemArray[0]}";
+                    addedBranch.Tag = branch.ItemArray[0];
+                    addedBranch.Location = new Point(10, 10);
+                    addedBranch.ForeColor = Color.Black;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.logger.Error($"GetCourses Error Hata Kodu: 5004 ex.message: {ex.Message} ex.stacktrace: {ex.StackTrace}");
+                MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 5004", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
