a9f1287964ca8cbb0f862c1d19a0b96780304fef
Emre Cüni <cuniiemre@gmail.com>
2025-03-16 11:31:48 +0300

    birth dates form added

---
+++ BIGADIC_COURSE/FormBirthDates.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BIGADIC_COURSE
+{
+    partial class FormBirthDates
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormBirthDates));
+            this.listViewBirthDate = new System.Windows.Forms.ListView();
+            this.columnHeaderTckn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSurname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderPhone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderBirthDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStripExport = new System.Windows.Forms.MenuStrip();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToExcelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToPdfToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.columnHeaderId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStripExport.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewBirthDate
+            // 
+            this.listViewBirthDate.BackColor = System.Drawing.Color.Silver;
+            this.listViewBirthDate.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderId,
+            this.columnHeaderTckn,
+            this.columnHeaderName,
+            this.columnHeaderSurname,
+            this.columnHeaderPhone,
+            this.columnHeaderBirthDate});
+            this.listViewBirthDate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewBirthDate.HideSelection = false;
+            this.listViewBirthDate.Location = new System.Drawing.Point(0, 28);
+            this.listViewBirthDate.Name = "listViewBirthDate";
+            this.listViewBirthDate.Size = new System.Drawing.Size(769, 371);
+            this.listViewBirthDate.TabIndex = 0;
+            this.listViewBirthDate.UseCompatibleStateImageBehavior = false;
+            this.listViewBirthDate.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderTckn
+            // 
+            this.columnHeaderTckn.Text = "TC. No";
+            this.columnHeaderTckn.Width = 110;
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "Adı";
+            this.columnHeaderName.Width = 150;
+            // 
+            // columnHeaderSurname
+            // 
+            this.columnHeaderSurname.Text = "Soyadı";
+            this.columnHeaderSurname.Width = 150;
+            // 
+            // columnHeaderPhone
+            // 
+            this.columnHeaderPhone.Text = "Telefon";
+            this.columnHeaderPhone.Width = 125;
+            // 
+            // columnHeaderBirthDate
+            // 
+            this.columnHeaderBirthDate.Text = "Doğum Tarihi";
+            this.columnHeaderBirthDate.Width = 130;
+            // 
+            // menuStripExport
+            // 
+            this.menuStripExport.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuStripExport.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToolStripMenuItem});
+            this.menuStripExport.Location = new System.Drawing.Point(0, 0);
+            this.menuStripExport.Name = "menuStripExport";
+            this.menuStripExport.Size = new System.Drawing.Size(769, 28);
+            this.menuStripExport.TabIndex = 1;
+            this.menuStripExport.Text = "menuStrip1";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToExcelToolStripMenuItem,
+            this.exportToPdfToolStripMenuItem});
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(101, 24);
+            this.exportToolStripMenuItem.Text = "Dışarı Aktar";
+            // 
+            // exportToExcelToolStripMenuItem
+            // 
+            this.exportToExcelToolStripMenuItem.Name = "exportToExcelToolStripMenuItem";
+            this.exportToExcelToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.exportToExcelToolStripMenuItem.Text = "Excel\'e Aktar";
+            this.exportToExcelToolStripMenuItem.Click += new System.EventHandler(this.exportToExcelToolStripMenuItem_Click);
+            // 
+            // exportToPdfToolStripMenuItem
+            // 
+            this.exportToPdfToolStripMenuItem.Name = "exportToPdfToolStripMenuItem";
+            this.exportToPdfToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.exportToPdfToolStripMenuItem.Text = "Pdf\'e Aktar";
+            this.exportToPdfToolStripMenuItem.Click += new System.EventHandler(this.exportToPdfToolStripMenuItem_Click);
+            // 
+            // columnHeaderId
+            // 
+            this.columnHeaderId.Text = "ID";
+            this.columnHeaderId.Width = 100;
+            // 
+            // FormBirthDates
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(769, 399);
+            this.Controls.Add(this.listViewBirthDate);
+            this.Controls.Add(this.menuStripExport);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStripExport;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "FormBirthDates";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Doğum Günleri";
+            this.Load += new System.EventHandler(this.FormBirthDates_Load);
+            this.menuStripExport.ResumeLayout(false);
+            this.menuStripExport.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewBirthDate;
+        private System.Windows.Forms.ColumnHeader columnHeaderTckn;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderSurname;
+        private System.Windows.Forms.ColumnHeader columnHeaderPhone;
+        private System.Windows.Forms.ColumnHeader columnHeaderBirthDate;
+        private System.Windows.Forms.MenuStrip menuStripExport;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToExcelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToPdfToolStripMenuItem;
+        private System.Windows.Forms.ColumnHeader columnHeaderId;
+    }
+}
---
+++ BIGADIC_COURSE/FormBirthDates.cs
@@ -0,0 +1,200 @@
+﻿using BIGADIC_COURSE.Classes;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BIGADIC_COURSE
+{
+    public partial class FormBirthDates : Form
+    {
+        public FormBirthDates(List<Register> birthDates)
+        {
+            InitializeComponent();
+            _birthDates = birthDates;
+        }
+
+        List<Register> _birthDates;
+        StringBuilder idStr = new StringBuilder();
+
+        private void FormBirthDates_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (Register birthDate in _birthDates)
+                {
+                    idStr.Clear();
+                    if (birthDate.ID < 10)
+                        idStr.Append($"GKM000{birthDate.ID}");
+                    else if (birthDate.ID < 100)
+                        idStr.Append($"GKM00{birthDate.ID}");
+                    else if (birthDate.ID < 1000)
+                        idStr.Append($"GKM0{birthDate.ID}");
+
+                    ListViewItem item = new ListViewItem(idStr.ToString());
+                    item.SubItems.Add(birthDate.TCKN);
+                    item.SubItems.Add(birthDate.NAME);
+                    item.SubItems.Add(birthDate.SURNAME);
+                    item.SubItems.Add(birthDate.PHONE);
+                    item.SubItems.Add(birthDate.BIRTHDATE.ToString("dd-MM-yyyy"));
+                    listViewBirthDate.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.logger.Error($"FormBirthDates_Load Error Hata Kodu: 9000 ex.message: {ex.Message} ex.stacktrace: {ex.StackTrace}");
+                MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 9000", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void exportToExcelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listViewBirthDate.Items.Count == 0) // listenen hiçbir kayıt yoksa onay ister
+                {
+                    DialogResult dialogResult = MessageBox.Show("Listenen Hiçbir Kayıt Yok. Devam Etmek İstiyor Musunuz?", "SORU", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                    if (dialogResult == DialogResult.Yes && ExportToExcel())
+                        MessageBox.Show("Kayıtlar Excel Dosyasına Aktarıldı.\n\nDosya Açılıyor...", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else if (ExportToExcel())
+                    MessageBox.Show("Kayıtlar Excel Dosyasına Aktarıldı.\n\nDosya Açılıyor...", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                Log.logger.Error($"exportExcelToolStripMenuItem_Click Error Hata Kodu: 9001 ex.message: {ex.Message} ex.stacktrace: {ex.StackTrace}");
+                MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 9001", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void exportToPdfToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listViewBirthDate.Items.Count == 0) // listenen hiçbir kayıt yoksa onay ister
+                {
+                    DialogResult dialogResult = MessageBox.Show("Listenen Hiçbir Kayıt Yok. Devam Etmek İstiyor Musunuz?", "SORU", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                    if (dialogResult == DialogResult.Yes && ExportToPdf())
+                        MessageBox.Show("Kayıtlar Pdf Dosyasına Aktarıldı.\n\nDosya Açılıyor...", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (ExportToPdf())
+                    MessageBox.Show("Kayıtlar Pdf Dosyasına Aktarıldı.\n\nDosya Açılıyor...", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                Log.logger.Error($"exportExcelToolStripMenuItem_Click Error Hata Kodu: 9002 ex.message: {ex.Message} ex.stacktrace: {ex.StackTrace}");
+                MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 9002", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ExportToExcel()
+        {
+            try
+            {
+                string excelPath = Application.StartupPath + "\\ExportExcel\\";
+                if (!Directory.Exists(excelPath))
+                    Directory.CreateDirectory(excelPath);
+
+                excelPath += $"birthDate_{DateTime.Now.Date:yyyy_MM_dd}.xlsx";
+                int row = 2;
+
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.Commercial;
+
+                using (var package = new ExcelPackage(new FileInfo(excelPath)))
+                {
+                    var worksheet = package.Workbook.Worksheets.Add($"{DateTime.Now:HH_mm_ss}");
+
+                    worksheet.Cells[1, 1].Value = "ID";
+                    worksheet.Cells[1, 2].Value = "T.C. No";
+                    worksheet.Cells[1, 3].Value = "Adı";
+                    worksheet.Cells[1, 4].Value = "Soyadı";
+                    worksheet.Cells[1, 5].Value = "Telefon";
+                    worksheet.Cells[1, 6].Value = "Doğum Tarihi";
+
+                    for (int i = 0; i < listViewBirthDate.Items.Count; i++)
+                    {
+                        worksheet.Cells[row, 1].Value = listViewBirthDate.Items[i].SubItems[0].Text;
+                        worksheet.Cells[row, 2].Value = listViewBirthDate.Items[i].SubItems[1].Text;
+                        worksheet.Cells[row, 3].Value = listViewBirthDate.Items[i].SubItems[2].Text;
+                        worksheet.Cells[row, 4].Value = listViewBirthDate.Items[i].SubItems[3].Text;
+                        worksheet.Cells[row, 5].Value = listViewBirthDate.Items[i].SubItems[4].Text;
+                        worksheet.Cells[row++, 6].Value = listViewBirthDate.Items[i].SubItems[5].Text;
+                    }
+                    package.Save();
+                }
+                Process.Start(excelPath);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.logger.Error($"ExportExcel Error Hata Kodu: 9003 ex.message: {ex.Message} ex.stacktrace: {ex.StackTrace}");
+                MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 9003", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private bool ExportToPdf()
+        {
+            try
+            {
+                string pdfPath = Application.StartupPath + "\\ExportPdf\\";
+                if (!Directory.Exists(pdfPath))
+                    Directory.CreateDirectory(pdfPath);
+
+                pdfPath += $"birthDate_{DateTime.Now:yyyy_MM_dd_HH_mm_ss}.pdf";
+
+                using (PdfWriter writer = new PdfWriter(new FileInfo(pdfPath)))
+                {
+                    using (PdfDocument pdf = new PdfDocument(writer))
+                    {
+                        iText.Layout.Document document = new iText.Layout.Document(pdf);
+
+                        PdfFont font = PdfFontFactory.CreateFont("C:/Windows/Fonts/arial.ttf", PdfEncodings.IDENTITY_H);
+
+                        // Tabloyu oluşturuyoruz ve sütun genişliklerini ayarlıyoruz
+                        float[] columnWidths = { 1, 2, 3, 3, 2, 2 }; // Sütun genişlikleri
+                        Table table = new Table(columnWidths);
+
+                        table.AddCell("ID").SetFont(font).SetFontSize(13);
+                        table.AddCell("T.C. No").SetFont(font).SetFontSize(13);
+                        table.AddCell("Adı").SetFont(font).SetFontSize(13);
+                        table.AddCell("Soyadı").SetFont(font).SetFontSize(13);
+                        table.AddCell("Telefon").SetFont(font).SetFontSize(13);
+                        table.AddCell("Doğum Tarihi").SetFont(font).SetFontSize(13);
+
+                        for (int i = 0; i < listViewBirthDate.Items.Count; i++)
+                        {
+                            table.AddCell(listViewBirthDate.Items[i].SubItems[0].Text).SetFont(font).SetFontSize(13);
+                            table.AddCell(listViewBirthDate.Items[i].SubItems[1].Text).SetFont(font).SetFontSize(13);
+                            table.AddCell(listViewBirthDate.Items[i].SubItems[2].Text).SetFont(font).SetFontSize(13);
+                            table.AddCell(listViewBirthDate.Items[i].SubItems[3].Text).SetFont(font).SetFontSize(13);
+                            table.AddCell(listViewBirthDate.Items[i].SubItems[4].Text).SetFont(font).SetFontSize(13);
+                            table.AddCell(listViewBirthDate.Items[i].SubItems[5].Text).SetFont(font).SetFontSize(13);
+                        }
+
+                        document.Add(table);
+                        document.Close();
+                    }
+                }
+
+                Process.Start(pdfPath);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log.logger.Error($"ExportToPdf Error Hata Kodu: 9004 ex.message: {ex.Message} ex.stacktrace: {ex.StackTrace}");
+                MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 9004", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+    }
+}
