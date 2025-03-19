4a94b5516b508d23bd770575c60f10e68a96532e
Emre Cüni <cuniiemre@gmail.com>
2025-03-19 22:42:50 +0300

    add excel class

---
+++ BIGADIC_COURSE/Classes/ExportExcel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIGADIC_COURSE.Classes
+{
+    public class ExportExcel
+    {
+        private string _excelPath = null!;
+        private string _excelName = null!;
+        private int row = 6;
+
+        public ExportExcel(string excelPath, string excelName)
+        {
+            _excelPath = excelPath;
+
+            if (!Directory.Exists(_excelPath))
+                Directory.CreateDirectory(_excelPath);
+            _excelName = excelName;
+
+            _excelPath += "\\" + _excelName;
+        }
+
+        // excel dosyasını oluşturmayı yaz
+    }
+}
