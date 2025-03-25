c0937c6289f9593682df97790c4a5f4db51bd69a
Emre Cüni <cuniiemre@gmail.com>
2025-03-25 18:12:03 +0300

    personel branch added

--- BIGADIC_COURSE/FormAddPersonel.Designer.cs
+++ BIGADIC_COURSE/FormAddPersonel.Designer.cs
@@ -29,23 +29,78 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAddPersonelBranch));
+            groupBoxAddBranch = new GroupBox();
+            buttonAddBranch = new Button();
+            textBoxBranch = new TextBox();
+            labelBranch = new Label();
+            groupBoxAddBranch.SuspendLayout();
             SuspendLayout();
+            // 
+            // groupBoxAddBranch
+            // 
+            groupBoxAddBranch.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            groupBoxAddBranch.BackColor = Color.Transparent;
+            groupBoxAddBranch.Controls.Add(buttonAddBranch);
+            groupBoxAddBranch.Controls.Add(textBoxBranch);
+            groupBoxAddBranch.Controls.Add(labelBranch);
+            groupBoxAddBranch.Location = new Point(12, 12);
+            groupBoxAddBranch.Name = "groupBoxAddBranch";
+            groupBoxAddBranch.Size = new Size(307, 121);
+            groupBoxAddBranch.TabIndex = 1;
+            groupBoxAddBranch.TabStop = false;
+            // 
+            // buttonAddBranch
+            // 
+            buttonAddBranch.BackColor = Color.White;
+            buttonAddBranch.Font = new Font("Consolas", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            buttonAddBranch.ForeColor = Color.Black;
+            buttonAddBranch.Location = new Point(22, 63);
+            buttonAddBranch.Name = "buttonAddBranch";
+            buttonAddBranch.Size = new Size(268, 45);
+            buttonAddBranch.TabIndex = 18;
+            buttonAddBranch.Text = "Ekle";
+            buttonAddBranch.UseVisualStyleBackColor = false;
+            buttonAddBranch.Click += buttonAddBranch_Click;
+            // 
+            // textBoxBranch
+            // 
+            textBoxBranch.Location = new Point(144, 25);
+            textBoxBranch.Name = "textBoxBranch";
+            textBoxBranch.Size = new Size(146, 27);
+            textBoxBranch.TabIndex = 1;
+            // 
+            // labelBranch
+            // 
+            labelBranch.AutoSize = true;
+            labelBranch.Location = new Point(18, 31);
+            labelBranch.Name = "labelBranch";
+            labelBranch.Size = new Size(78, 20);
+            labelBranch.TabIndex = 0;
+            labelBranch.Text = "Branş Adı:";
             // 
             // FormAddPersonelBranch
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkGray;
-            ClientSize = new Size(740, 512);
+            ClientSize = new Size(334, 149);
+            Controls.Add(groupBoxAddBranch);
             Font = new Font("Segoe UI Semibold", 11.25F);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Margin = new Padding(4);
             Name = "FormAddPersonelBranch";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Personel Branşı Ekle";
+            groupBoxAddBranch.ResumeLayout(false);
+            groupBoxAddBranch.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBoxAddBranch;
+        private Button buttonAddBranch;
+        private TextBox textBoxBranch;
+        private Label labelBranch;
     }
 }
--- BIGADIC_COURSE/FormAddPersonel.cs
+++ BIGADIC_COURSE/FormAddPersonel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BIGADIC_COURSE.Classes;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static iText.IO.Image.Jpeg2000ImageData;
 
 namespace BIGADIC_COURSE
 {
@@ -15,6 +19,61 @@ namespace BIGADIC_COURSE
         public FormAddPersonelBranch()
         {
             InitializeComponent();
+        }
+
+        Sql sql = new();
+        StringBuilder query = new();
+        List<SqlParameter> parameters = new();
+
+        private async void buttonAddBranch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBoxBranch.Text != null)
+                {
+                    query.Clear();
+                    query.Append("IF NOT EXISTS (SELECT * FROM COURSES WHERE NAME = @Name) " +
+                        "BEGIN " +
+                        "INSERT INTO COURSES (NAME, TYPE, ADDITION_TIME) VALUES (@Name, @Type, GETDATE()) " +
+                        "END " +
+                        "ELSE " +
+                    "BEGIN " +
+                    "SELECT 'MEVCUT KAYIT BULUNMAKTADIR' " +
+                    "END ");
+
+                    parameters.Clear();
+                    parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar, 50) { Value = textBoxBranch.Text });
+                    parameters.Add(new SqlParameter("@Type", SqlDbType.TinyInt) { Value = 1 });
+                    int resultCode = await sql.EditData(query.ToString(), parameters);
+
+                    switch (resultCode)
+                    {
+                        case 0:
+                            Log.logger.Error("Sql Bağlantısı Açılamadı. Hata Kodu: 10001");
+                            MessageBox.Show("Bir Hata Oluştu. Tekrar Deneyiniz.\nHata Kodu: 10001", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            break;
+                        case 1:
+                            Log.logger.Info($"Branş Başarıyla Eklendi. {textBoxBranch.Text}");
+                            MessageBox.Show("Branş Başarıyla Eklendi.", "BİLGİ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            break;
+                        case 2:
+                            Log.logger.Warn("Branş zaten kayıtlı.");
+                            MessageBox.Show($"'{textBoxBranch.Text}' Branşı Zaten Kayıtlı.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            break;
+                        case -1:
+                            Log.logger.Error("Kayıt Eklenirken Bir Hata Oluştu. Hata Kodu: 10002");
+                            MessageBox.Show("Bir Hata Oluştu. Tekrar Deneyiniz.\nHata Kodu: 10002", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                    }
+                }
+                else
+                    MessageBox.Show("Lütfen Bir Branş Adı ve Tipini Giriniz.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                Log.logger.Error($"buttonAddBranch_Click Error Hata Kodu: 10000 ex.message: {ex.Message} ex.stacktrace: {ex.StackTrace}");
+                MessageBox.Show("Bir Hata Oluştu. Hata Kodu: 10000", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
